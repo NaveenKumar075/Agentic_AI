61dc3077f222e28c987f2bf605fa5205e23b6007
NaveenKumar075 <naveenkumarforjobs@gmail.com>
2025-02-04 18:41:39 +0530

    FEAT: Financial Agent script has been updated

--- Financial_Agent/main.py
+++ Financial_Agent/main.py
@@ -1,1 +1,40 @@
-print("Let's start the code here!")
+from agno.agent import Agent
+from agno.models.groq import Groq
+from agno.tools.duckduckgo import DuckDuckGoTools
+from agno.tools.yfinance import YFinanceTools
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+groq_api_key = os.getenv("GROQ_API_KEY")
+
+web_agent = Agent(
+    name="Web Agent",
+    role="Search the web for information",
+    model=Groq(id="llama-3.3-70b-versatile", api_key=groq_api_key),
+    tools=[DuckDuckGoTools()],
+    instructions="Always include sources",
+    show_tool_calls=True,
+    markdown=True,
+)
+
+finance_agent = Agent(
+    name="Finance Agent",
+    role="Get financial data",
+    model=Groq(id="llama-3.3-70b-versatile", api_key=groq_api_key),
+    tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, company_info=True)],
+    instructions="Use tables to display data",
+    show_tool_calls=True,
+    markdown=True,
+)
+
+agent_team = Agent(
+    team=[web_agent, finance_agent],
+    model=Groq(id="llama-3.3-70b-versatile", api_key=groq_api_key),
+    instructions=["Always include sources", "Use tables to display data"],
+    show_tool_calls=True,
+    markdown=True,
+)
+
+agent_team.print_response("What is the ITC share market status now?", stream=True) # What's the market outlook and financial performance of Nvidia? | What is Agentic AI and what are AI trends in 2025?
